493a6094b248159b9d3b7055f34a6ae4cf3a0c37
lanjudev16 <lanjudev16@gmail.com>
2023-09-25 23:24:30 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "./features/Counter";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-red-200">React Developer</h2>
+      <Counter></Counter>
+    </div>
+  );
+};
+
+export default App;
