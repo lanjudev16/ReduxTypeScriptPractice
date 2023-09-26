be8aee7afdb156ed8978bee828bb7efce47109f7
lanjudev16 <lanjudev16@gmail.com>
2023-09-26 17:19:07 +0600

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { increment } from "./features/webTheme/themeSlice";
 const App = () => {
   const theme=useSelector((state: RootState) => state.theme.value)
+
   const containerStyle = {
     background: theme ? 'white' : 'black',
     color: theme ? 'black' : 'white',
@@ -12,10 +13,12 @@ const App = () => {
   
   const dispatch=useDispatch()
   return (
-    <div style={containerStyle} >
+    <div style={containerStyle} className="flex items-center justify-center" >
+      <div>
       <h2 >React Developer</h2>
-    
-      <button onClick={()=>dispatch(increment())}>Toggle</button>
+       
+       <button onClick={()=>dispatch(increment())}>Toggle</button>
+      </div>
     </div>
   );
 };
